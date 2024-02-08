1335999eb3c24f7bb357030643dc0ce5307666a8
GianniCrux <gianni.disti@gmail.com>
2024-02-08 15:00:54 +0100

    aggiunta dei file nella directory

---
+++ src/Components/customHeader/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
+
+
+
+const CustomHeader = ({ chat }) => {
+
+ 
+  
+
+  return (
+    <div className="chat-header">
+        <div className="flexbetween">
+            <ChatBubbleLeftRightIcon className="icon-chat" />
+            <h3 className="header-text">{chat.title ? chat.title : 'Default Title'}</h3>
+        </div>
+        <div className="flexbetween">
+            <PhoneIcon className="icon-phone" />
+            <p className="header-text"> {chat.description} </p>
+            
+        </div>
+    </div>
+  )
+}
+
+CustomHeader.propTypes = {
+  chat: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }),
+};
+
+export default CustomHeader
